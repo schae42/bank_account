808fd5d345e45d2432ac2a10a59798cc979d9978
schae42 <chaechan@outlook.com>
2020-09-29 15:38:54 -0700

    bank-account

--- bankAccount.py
+++ bankAccount.py
@@ -18,7 +18,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Current {self.name} Balance: {self.balance}")
+        print(f"Current {self.name} Balance: ${self.balance}")
         return self
 
     def yield_interest(self):
@@ -30,8 +30,8 @@ checking = BankAccount('Checking',0.001)
 savings = BankAccount('Savings',bal=1000)
 
 #print beginning balances
-print(f"Beginning Checking Balance: {checking.balance}")
-print(f"Beginning Savings Balance: {savings.balance}")
+print(f"Beginning Checking Balance: ${checking.balance}")
+print(f"Beginning Savings Balance: ${savings.balance}")
 
 checking.deposit(100).deposit(100).deposit(100).withdraw(40).yield_interest().display_account_info()
 savings.deposit(200).deposit(200).withdraw(30).withdraw(50).withdraw(90).withdraw(100).yield_interest().display_account_info()
